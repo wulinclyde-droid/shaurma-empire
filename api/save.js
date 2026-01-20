3219e0fe30203f00969c6e679735e3a255ab754e
wulinclyde-droid <wulin.clyde@gmail.com>
2026-01-20 13:13:56 +0300

    Update save.js

--- api/save.js
+++ api/save.js
@@ -42,7 +42,7 @@ export default async function handler(req, res) {
       },
       body: JSON.stringify({
         user_id: String(userId),
-         safeData
+        data: safeData
       })
     });
 
